4d7cfaed1169ca80036b2e517c91157bcd0957ce
Dinushika Gedarawattage <106918606+D2109625@users.noreply.github.com>
2024-07-08 20:59:47 -0700

    prac4q2.scala

--- prac4q2.scala
+++ prac4q2.scala
@@ -1,11 +1,26 @@
-def patternmatch(x: Int): String = x match {
-  case x if x <= 0 => "Negative/Zero"
-  case x if x % 2 == 0 => "Even"
-  case x if x % 2 == 1 => "Odd"
-}
+object Main {
+  def toUpper(name: String): String = {
+    name.toUpperCase
+  }
 
-def main(args: Array[String]): Unit = {
-  println("Enter the number:")
-  val number = scala.io.StdIn.readInt()
-  println(patternmatch(number))
+  def toLower(name: String): String = {
+    name.toLowerCase
+  }
+
+  def formatName(name: String)(f: String => String): String = {
+    f(name)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val name1 = "Benny"
+    val name2 = "Niroshan"
+    val name3 = "Saman"
+    val name4 = "Kumara"
+
+    println(formatName(name1)(toUpper))
+
+    println(formatName(name2.substring(0, 2))(toUpper) + formatName(name2.substring(2))(toLower))
+    println(formatName(name3)(toLower))
+    println(formatName(name4.substring(0, 1))(toUpper) + formatName(name4.substring(1, 5))(toLower) + formatName(name4.substring(5))(toUpper))
+  }
 }
