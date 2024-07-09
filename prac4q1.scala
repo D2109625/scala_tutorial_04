de7cbe016f5f3b561cf417f01d3769c36dd30ce2
Dinushika Gedarawattage <106918606+D2109625@users.noreply.github.com>
2024-07-08 19:04:12 -0700

    prac4q1.scala

--- prac4q1.scala
+++ prac4q1.scala
@@ -1,16 +1,71 @@
-object ReverseString {
-  def stringReverser(s: String): String = {
-    if (s.isEmpty) {
-      "" 
-    } else {
-      stringReverser(s.tail) + s.head 
+def displayInventory(item: Array[String], quantity: Array[Int]) = {
+    print("Inventory\n")
+    for(i <- 0 to item.length-1) {
+        print(item(i) + " - ")
+        print(quantity(i))
+        print("\n")
     }
-  }
-  
-  def main(args: Array[String]): Unit = {
-    val string = "functional programming"
-    val reversedString = stringReverser(string)
-    println("Original string: " + string)
-    println("Reversed string: " + reversedString)
-  }
+}
+
+def restockItem(item: Array[String], quantity: Array[Int], newQuantity: Int) = {
+    var flag = false
+
+    for(i <- 0 to item.length-1) {
+        if(item(i) == "Rice") {
+            flag = true
+            var index = i
+            quantity(i) += newQuantity 
+        }
+    }
+
+    if(flag == false) {
+        print("Item not found")
+    }
+
+    print("\n\nRestock Updated Inventory\n")
+    for(i <- 0 to item.length-1) {
+        print(item(i) + " - ")
+        print(quantity(i))
+        print("\n")
+    }
+
+}
+
+def sellItem(item: Array[String], quantity: Array[Int], sellQuantity: Int) = {
+    var flag = false
+
+    for(i <- 0 to item.length-1) {
+        if(item(i) == "suger") {
+            flag = true
+            var index = i 
+                if(quantity(i) < sellQuantity) {
+                    print("Quantity is not enough");
+                } else {
+                    quantity(i) -= sellQuantity
+                }
+        }
+    }
+
+    if(flag == false) {
+        print("Item not found")
+    }
+
+    print("\n\nSell Updated Inventory\n")
+    for(i <- 0 to item.length-1) {
+        print(item(i) + " - ")
+        print(quantity(i))
+        print("\n")
+    }
+}
+
+def main(args: Array[String]) = {
+    var item = Array("Book","Sugar","Rice","Dhal")
+    var quantity = Array(50,47,20,35)
+
+    displayInventory(item, quantity)
+
+    restockItem(item, quantity, 10) 
+
+    sellItem(item, quantity, 3)   
+
 }
